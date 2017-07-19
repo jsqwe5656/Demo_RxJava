3f28db2fdf32827d9db6190131cb7937469a313a
zbf <516845590@qq.com>
2017-07-20 00:06:02 +0800

    Fragment事例1

--- fragment/src/main/java/com/example/fragment/MainActivity.java
+++ fragment/src/main/java/com/example/fragment/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.fragment;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +13,17 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentInit();
+
+    }
+
+    private void fragmentInit()
+    {
+        //管理碎片
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        //执行碎片事务操作
+
+        fragmentTransaction.commit();
     }
 }
